04c9956c30d6bcc102d162b0c8e16130092c52ba
Nick Andreyeu <54830989+nickSpeaks@users.noreply.github.com>
2023-12-19 23:18:29 -0800

    Update ftracker.go
    correct typos: lenght -> length

--- ftracker.go
+++ ftracker.go
@@ -133,7 +133,7 @@ func swimmingMeanSpeed(lengthPool, countPool int, duration float64) float64 {
 // countPool int — сколько раз пользователь переплыл бассейн.
 // duration float64 — длительность тренировки в часах.
 // weight float64 — вес пользователя.
-func SwimmingSpentCalories(lenghtPool, countPool int, duration, weight float64) float64 {
+func SwimmingSpentCalories(lengthPool, countPool int, duration, weight float64) float64 {
 	// (СредняяСкоростьВКм/ч + 1.1) * 2 * ВесСпортсменаВКг * ВремяТренеровкиВЧасах
-	return (swimmingMeanSpeed(lenghtPool, countPool, duration) * swimmingCaloriesMeanSpeedShift) * swimmingCaloriesWeightMultiplier * weight * duration
+	return (swimmingMeanSpeed(lengthPool, countPool, duration) * swimmingCaloriesMeanSpeedShift) * swimmingCaloriesWeightMultiplier * weight * duration
 }
