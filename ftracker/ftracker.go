755c4f4fda9d1f933f7a76300429bf9ab10545a0
Vladimir Marunin <vladimir.marunin@revjet.com>
2024-01-26 01:23:57 +0300

    111

--- ftracker/ftracker.go
+++ ftracker/ftracker.go
@@ -80,7 +80,7 @@ const (
 // weight float64 — вес пользователя.
 // duration float64 — длительность тренировки в часах.
 func RunningSpentCalories(action int, weight, duration float64) float64 {
-	// ваш код здесь
+	// ваш код здесь, но не работает.
 	return ((runningCaloriesMeanSpeedMultiplier * meanSpeed(action, duration) * runningCaloriesMeanSpeedShift) * weight / mInKm * duration * minInH)
 }
 
