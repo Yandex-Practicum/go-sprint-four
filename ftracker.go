291b0559692e487ddca3c50a5d04f4de8aeec03b
oli_khi <Brandonred1@icloud.com>
2024-02-04 14:35:32 +0300

    second commit

--- ftracker.go
+++ ftracker.go
@@ -102,8 +102,8 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	walkingSpentCalories := ((walkingCaloriesWeightMultiplier*weight + (math.Pow((meanSpeed(action, duration)*kmhInMsec), 2)/height)*walkingSpeedHeightMultiplier*
-		weight) * duration * minInH)
+	walkingSpentCalories := ((walkingCaloriesWeightMultiplier*weight + (math.Pow((meanSpeed(action, duration)*kmhInMsec), 2)/(height/100))*
+		walkingSpeedHeightMultiplier*weight) * duration * minInH)
 	return walkingSpentCalories
 }
 
