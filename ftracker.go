e3208ea1fac43419c341acb4c3f0fc0fbc394c5c
Aram Mkhitaryan <mkhitaryanaram2004@gmail.com>
2024-02-23 05:11:41 +0300

    Second commit

--- ftracker.go
+++ ftracker.go
@@ -59,7 +59,7 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	case trainingType == "Плавание":
 		distance := distance(action)                                               // вызовите здесь необходимую функцию
-		speed := meanSpeed(action, duration)                                       // вызовите здесь необходимую функцию
+		speed := swimmingMeanSpeed(lengthPool, countPool, duration)                // вызовите здесь необходимую функцию
 		calories := SwimmingSpentCalories(lengthPool, countPool, duration, weight) // вызовите здесь необходимую функцию
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	default:
@@ -105,12 +105,11 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	SecInH := math.Pow(minInH, 2)
-	MeanSpeedInMs := meanSpeed(action, duration) * (mInKm / SecInH)
+	MeanSpeedInMs := meanSpeed(action, duration) * kmhInMsec
 	MeanSpeedInMsSqrt := math.Pow(MeanSpeedInMs, 2)
-	// heightInM := height / 100
+	heightInM := height / cmInM
 	return ((walkingCaloriesWeightMultiplier*weight +
-		(MeanSpeedInMsSqrt/height)*
+		(MeanSpeedInMsSqrt/heightInM)*
 			walkingSpeedHeightMultiplier*weight) * duration * minInH)
 }
 
@@ -145,5 +144,6 @@ func swimmingMeanSpeed(lengthPool, countPool int, duration float64) float64 {
 func SwimmingSpentCalories(lengthPool, countPool int, duration, weight float64) float64 {
 	// ваш код здесь
 	SwimmingMeanSpeed := swimmingMeanSpeed(lengthPool, countPool, duration)
-	return (SwimmingMeanSpeed + swimmingCaloriesMeanSpeedShift) * 2 * weight * duration
+	return (SwimmingMeanSpeed + swimmingCaloriesMeanSpeedShift) *
+		swimmingCaloriesWeightMultiplier * weight * duration
 }
